b1d04e3ab8602a672c0aa2c17cc897ac719dc787
Jayanth <jayanthfaraday@gmail.com>
2024-04-12 22:05:36 -0700

    ceaser cipher day 8 challenge

---
+++ day8.py
@@ -0,0 +1,66 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+  
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""  
+
+print(logo)
+
+def encrypt(text, shift):
+    encrypted_text = ""
+    for i in text:
+        if i.isalpha():
+           num= alphabet.index(i)
+           num=(num+shift)%26
+           encrypted_text+=alphabet[num] 
+        else:
+            encrypted_text+=" "   
+        
+    print(encrypted_text)      
+
+def decrypt(text,shift):
+    decrypted_text=""
+    for i in text:
+        if i.isalpha():
+            num=alphabet.index(i)
+            num=abs(num-shift)%26 
+            decrypted_text+=alphabet[num]
+        else:
+            decrypted_text+=" "    
+        
+    print(decrypted_text)         
+
+
+
+choice='yes'
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+while choice=='yes':
+    if direction == "encode":
+        encrypt(text,shift)
+    else:
+        decrypt(text,shift)   
+    choice=input("type 'yes' if you wanna do again. Type 'no' otherwise.")
+    if choice=='yes':
+        direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+        text = input("Type your message:\n").lower()
+        shift = int(input("Type the shift number:\n")) 
+
+print("Good Bye.")           
