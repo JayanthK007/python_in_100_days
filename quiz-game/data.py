015856450892d3e22ec3bb890bc51db0a6221cfc
Jayanth <jayanthfaraday@gmail.com>
2024-04-23 22:20:09 -0700

    day 17 challenge

---
+++ quiz-game/data.py
@@ -0,0 +1,104 @@
+question_data = [
+
+{
+"type": "boolean",
+"difficulty": "easy",
+"category": "Mythology",
+"question": "According to Greek Mythology, Zeus can control lightning.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"type": "boolean",
+"difficulty": "hard",
+"category": "Science: Mathematics",
+"question": "The binary number &quot;101001101&quot; is equivalent to the Decimal number &quot;334&quot;",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"type": "boolean",
+"difficulty": "medium",
+"category": "Entertainment: Television",
+"question": "An episode of &quot;The Simpsons&quot; is dedicated to Moe Szyslak&#039;s bar rag.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"type": "boolean",
+"difficulty": "hard",
+"category": "Entertainment: Video Games",
+"question": "All of these maps were in &quot;Tom Clancy&#039;s Rainbow Six Siege&quot; on its initial release: House, Clubhouse, Border, Consulate.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"type": "boolean",
+"difficulty": "medium",
+"category": "Entertainment: Video Games",
+"question": "In World of Warcraft lore, Thrall is the original wielder of &quot;Doomhammer&quot;.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"type": "boolean",
+"difficulty": "hard",
+"category": "Entertainment: Music",
+"question": "The song &quot;Mystery Train&quot; was released by artist &quot;Little Junior&#039;s Blue Flames&quot; in 1953.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"type": "boolean",
+"difficulty": "easy",
+"category": "Entertainment: Video Games",
+"question": "Peter Molyneux was the founder of Bullfrog Productions.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"type": "boolean",
+"difficulty": "easy",
+"category": "Entertainment: Video Games",
+"question": "Deus Ex (2000) does not feature the World Trade Center because it was destroyed by terrorist attacks according to the game&#039;s plot.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+},
+{
+"type": "boolean",
+"difficulty": "easy",
+"category": "General Knowledge",
+"question": "Dihydrogen Monoxide is a dangerous chemical.",
+"correct_answer": "False",
+"incorrect_answers": [
+"True"
+]
+},
+{
+"type": "boolean",
+"difficulty": "hard",
+"category": "Entertainment: Music",
+"question": "The singer Billie Holiday was also known as &quot;Lady Day&quot;.",
+"correct_answer": "True",
+"incorrect_answers": [
+"False"
+]
+}
+]
+
