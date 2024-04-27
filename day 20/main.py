29b1bc6a26fdd8438420f4cc8a9f67d84b765b95
Jayanth <jayanthfaraday@gmail.com>
2024-04-26 18:27:14 -0700

    day 20 challenge

---
+++ day 20/main.py
@@ -0,0 +1,26 @@
+from turtle import Turtle,Screen
+import time
+from snake import Snake
+
+screen=Screen()
+screen.setup(width=600,height=600)
+screen.bgcolor('black')
+screen.title('My Snake Game')
+screen.tracer(0)
+
+my_snake=Snake()
+
+screen.listen()
+screen.onkey(my_snake.up,"Up")
+screen.onkey(my_snake.down,"Down")
+screen.onkey(my_snake.left,"Left")
+screen.onkey(my_snake.right,"Right")
+
+is_game_on=True
+
+while is_game_on:
+    screen.update()
+    time.sleep(0.1)
+    my_snake.move()    
+
+screen.exitonclick()
